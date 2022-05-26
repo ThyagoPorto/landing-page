6da05099424451cac5ec49b36d0c6df69fac5696
Thyago Porto <100157505+ThyagoPorto@users.noreply.github.com>
2022-05-26 18:25:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayAbout() {
+  
+     var arrow = document.getElementById('animation');
+     arrow.classList.toggle('rotate');
+     var display = document.getElementById('about');
+     display.classList.toggle('active');
+
+}
+   
